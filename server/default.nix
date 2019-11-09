ba16649b4875272b9c13bac7029adbd3a658b519
Kevin Liu <kevin@kliu.io>
2019-11-09 10:22:57 -0500

    Add server

---
+++ server/default.nix
@@ -0,0 +1,10 @@
+# (see https://nixos.org/nix/)
+
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+    name = "cortex";
+    buildInputs = [
+        python3 python3Packages.pip python3Packages.setuptools libffi openssl
+    ];
+}
