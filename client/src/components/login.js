b570cea8b96d7bc28b6655cc5bf92b45d55522bf
Will Hemond <willhemond@scintillating.us>
2019-11-09 01:18:18 -0500

    initial lander

---
+++ client/src/components/login.js
@@ -0,0 +1,11 @@
+class bigLogin extends React.Component {
+    render () {
+        return (
+            <div classname="container">
+                <h1>Login</h1>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Hello />, mountnode);
