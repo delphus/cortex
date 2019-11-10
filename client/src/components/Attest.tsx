c586b37150bce0bfd412907233b7c8feb838ca04
Kevin Liu <kevin@kliu.io>
2019-11-10 06:27:12 -0500

    Get login with Fortmatic working

--- client/src/components/Attest.tsx
+++ client/src/components/Attest.tsx
@@ -8,14 +8,6 @@ function SubmitAttestation({ form }: FormComponentProps) {
   const { drizzle, readinessState } = useContext(DrizzleContext);
   const { getFieldDecorator, validateFieldsAndScroll } = form;
   const formItemLayout = {
-    labelCol: {
-      xs: { span: 24 },
-      sm: { span: 8 }
-    },
-    wrapperCol: {
-      xs: { span: 24 },
-      sm: { span: 16 }
-    }
   };
 
   return (
