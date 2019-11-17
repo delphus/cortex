0fd0f5c5fec2cf21a469897c1dcd7e324c92129e
Kevin Liu <kevin@kliu.io>
2019-11-17 00:17:19 -0500

    Hook up UI to backend

--- client/src/components/SubmitData.tsx
+++ client/src/components/SubmitData.tsx
@@ -1,69 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import * as api from "../util/submit-api";
+import { message } from "antd";
+
+const Center = styled.div`
+  text-align: center;
+`;
+//fix this action
+const SubmitDataForm = styled.form`
+  action="/action_page.php";
+  method = "post";
+`;
 
 export default function SubmitData() {
+  let adminLevel = "Administrator"; // CHANGE THIS LATER
+  let userList: string[] = ["Bob 1", "Bob 2", "Bob 3"]; // FIX THIS ALSO
 
-  let adminLevel = "Administrator"; //CHANGE THIS LATER
-  let userList: string[] = ["a", "b", "c"] //FIX THIS ALSO
-  let previousData: string[] = ["hi", "greetings", "hello"] //do this
+  const [data, setData] = useState("");
+  const [dec, setDec] = useState<string | null>(null);
 
-  const users = userList.map( (str) => <li>{str}</li>)
-  const prevData = previousData.map( (str) => <li>{str}</li>)
-   
-  const Center = styled.div`
-    text-align: center;
-  `
- //fix this action
-  const SubmitData = styled.form`
-    action="/action_page.php";
-    method = "post";
-  `
-  
+  const users = userList.map(str => <li key={str}>{str}</li>);
+
+  useEffect(() => {
+    (async () => {
+      const decrypted = await api.retrieve(
+        0,
+        localStorage.getItem("ciphertext")!
+      );
+      setDec(decrypted);
+    })();
+  }, [localStorage.getItem("ciphertext")]);
+
   return (
     <>
       <Center>
         <h2>Submit Data</h2>
-        <h2>Showing Data With Encryption Level: <b>{adminLevel}</b></h2>
+        <h2>
+          Showing Data With Encryption Level: <b>{adminLevel}</b>
+        </h2>
       </Center>
 
       <h3>Other users with {adminLevel} access: </h3>
       <ul>{users}</ul>
 
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
-      <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        
-      <SubmitData>
+      <SubmitDataForm
+        onSubmit={async e => {
+          e.preventDefault();
+          const ciphertext = await api.encrypt(data);
+          localStorage.setItem("ciphertext", ciphertext);
+          message.info(`Successfully NuCypherified text as ${ciphertext}`);
+          setData("");
+        }}
+      >
         Add Data:
-        <br/>
-        <input type="text" name="data" size={100}/>
-       
+        <br />
+        <input
+          type="text"
+          name="data"
+          size={100}
+          value={data}
+          onChange={e => setData(e.target.value)}
+        />
+        <br />
+        <br />
+        <input type="submit" value="Submit" />
+      </SubmitDataForm>
 
-        <br/>
-        <br/>
-        <input type="submit" value="Submit" name="data"/>
-      </SubmitData>
-
-      <br/>
-        <br/>
-        <br/>
-        <br/>
+      <br />
+      <br />
+      <br />
+      <br />
 
       <Center>
         <h3>Previously Submitted Data</h3>
       </Center>
 
-      <ul>
-        {prevData}
-      </ul>
-
-    
+      {dec || localStorage.getItem("ciphertext")}
     </>
-
-    
   );
 }
